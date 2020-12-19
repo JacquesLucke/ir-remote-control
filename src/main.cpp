65df99b38bb6d31a6010707e6c752517b79290bd
Jacques Lucke <mail@jlucke.com>
2020-12-19 21:14:43 +0100

    setup wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+/* Has to define WIFI_NAME and WIFI_PASSWORD. */
+#include "private_data.h"
 
 static void setupWiFiConnection() {
+  WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
+  Serial.begin(9600);
+  setupWiFiConnection();
 }
 
 void loop() {
