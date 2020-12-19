dce90959bae8be258227dae2f14a517b2b375454
Jacques Lucke <mail@jlucke.com>
2020-12-19 21:20:25 +0100

    start web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 
 /* Has to define WIFI_NAME and WIFI_PASSWORD. */
 #include "private_data.h"
+
+constexpr int web_server_port = 80;
+ESP8266WebServer web_server(web_server_port);
 
 static void setupWiFiConnection() {
   WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
@@ -16,9 +20,15 @@ static void setupWiFiConnection() {
   Serial.println(WiFi.localIP());
 }
 
+static void setupWebServer() {
+  web_server.begin();
+  Serial.printf("Started web server on port %d.\n", web_server_port);
+}
+
 void setup() {
   Serial.begin(9600);
   setupWiFiConnection();
+  setupWebServer();
 }
 
 void loop() {
